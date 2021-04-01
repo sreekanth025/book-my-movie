4bb6e0957deeb08d3238b5d1cc3d4df40673d6c0
sreekanth025 <vs25@iitbbs.ac.in>
2021-04-01 23:58:10 +0530

    Theatre model

---
+++ src/main/java/com/bookmymovie/models/Movie.java
@@ -0,0 +1,18 @@
+package com.bookmymovie.models;
+
+import java.sql.Date;
+
+public class Movie {
+	
+	String movie_id;
+	String title;
+	Double rating;
+	String certification;
+//	duration;
+	Date release_date;
+	String about;
+//	poster_link;
+	
+}
+
+// generate constructors, getters and setters after deciding datatypes
---
+++ src/main/java/com/bookmymovie/models/Theatre.java
@@ -0,0 +1,71 @@
+package com.bookmymovie.models;
+
+public class Theatre {
+
+	private String theatre_id;
+	private String screen_id;
+	private String name;
+	private String street;
+	private String city;
+	private Integer price;
+	
+	
+	public Theatre() {
+
+	}
+	
+	public Theatre(String theatre_id, String screen_id, String name, String street, String city, Integer price) {
+		this.theatre_id = theatre_id;
+		this.screen_id = screen_id;
+		this.name = name;
+		this.street = street;
+		this.city = city;
+		this.price = price;
+	}
+	
+	
+	public String getTheatre_id() {
+		return theatre_id;
+	}
+	public void setTheatre_id(String theatre_id) {
+		this.theatre_id = theatre_id;
+	}
+	public String getScreen_id() {
+		return screen_id;
+	}
+	public void setScreen_id(String screen_id) {
+		this.screen_id = screen_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Theatre [theatre_id=" + theatre_id + ", screen_id=" + screen_id + ", name=" + name + ", street="
+				+ street + ", city=" + city + ", price=" + price + "]";
+	}
+	
+}
